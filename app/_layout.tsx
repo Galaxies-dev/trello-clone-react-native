fad3f8250506310e6a86248ab6f4caa07103619c
Simon Grimm <saimon@devdactic.com>
2024-06-20 14:50:35 +0200

    Finish login UI.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const InitialLayout = () => {
   return (
@@ -13,10 +14,10 @@ const InitialLayout = () => {
 const RootLayoutNav = () => {
   return (
     <ActionSheetProvider>
-      <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         <StatusBar style="light" />
         <InitialLayout />
-      </>
+      </GestureHandlerRootView>
     </ActionSheetProvider>
   );
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,55 @@
 import { Colors } from '@/constants/Colors';
-import { Text, View, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useActionSheet } from '@expo/react-native-action-sheet';
+import * as WebBrowser from 'expo-web-browser';
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+  BottomSheetBackdrop,
+} from '@gorhom/bottom-sheet';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
+enum ModalType {
+  Login = 'login',
+  SignUp = 'signup',
+}
+
+const LOGIN_OPTIONS = [
+  {
+    text: 'Continue with Google',
+    icon: require('@/assets/images/login/google.png'),
+  },
+  {
+    text: 'Continue with Microsoft',
+    icon: require('@/assets/images/login/microsoft.png'),
+  },
+  {
+    text: 'Continue with Apple',
+    icon: require('@/assets/images/login/apple.png'),
+  },
+  {
+    text: 'Continue with Slack',
+    icon: require('@/assets/images/login/slack.png'),
+  },
+];
 
 export default function Index() {
   const { top } = useSafeAreaInsets();
   const { showActionSheetWithOptions } = useActionSheet();
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const snapPoints = useMemo(() => ['33%'], []);
+  const [authType, setAuthType] = useState<ModalType | null>(null);
 
   const openLink = async () => {
-    // TODO: Open in app browser
-    Linking.openURL('https://galaxies.dev');
+    WebBrowser.openBrowserAsync('https://galaxies.dev');
   };
 
   const openActionSheet = async () => {
-    console.log('Opening action sheet');
     const options = ['View support docs', 'Contact us', 'Cancel'];
-    // const destructiveButtonIndex = 0;
     const cancelButtonIndex = 2;
 
     showActionSheetWithOptions(
@@ -37,35 +71,80 @@ export default function Index() {
     );
   };
 
+  const showModal = async (type: ModalType) => {
+    setAuthType(type);
+    bottomSheetModalRef.current?.present();
+  };
+
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        opacity={0.2}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        {...props}
+        onPress={() => bottomSheetModalRef.current?.close()}
+      />
+    ),
+    []
+  );
+
   return (
-    <View style={[styles.container, { paddingTop: top + 30 }]}>
-      <Image source={require('@/assets/images/login.png')} style={styles.image} />
-      <Text style={styles.introText}>Move teamwork forward - even on the go</Text>
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity style={[styles.btn, { backgroundColor: 'white' }]}>
-          <Text style={[styles.btnText, { color: Colors.primary }]}>Log in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.btn]}>
-          <Text style={[styles.btnText, { color: '#fff' }]}>Sign Up</Text>
-        </TouchableOpacity>
+    <BottomSheetModalProvider>
+      <View style={[styles.container, { paddingTop: top + 30 }]}>
+        <Image source={require('@/assets/images/login/trello.png')} style={styles.image} />
+        <Text style={styles.introText}>Move teamwork forward - even on the go</Text>
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity
+            style={[styles.btn, { backgroundColor: 'white' }]}
+            onPress={() => showModal(ModalType.Login)}>
+            <Text style={[styles.btnText, { color: Colors.primary }]}>Log in</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.btn]} onPress={() => showModal(ModalType.SignUp)}>
+            <Text style={[styles.btnText, { color: '#fff' }]}>Sign Up</Text>
+          </TouchableOpacity>
 
-        <Text style={styles.description}>
-          By signing up, you agree to the{' '}
-          <Text style={styles.link} onPress={openLink}>
-            User Notice
-          </Text>{' '}
-          and{' '}
-          <Text style={styles.link} onPress={openLink}>
-            Privacy Policy
+          <Text style={styles.description}>
+            By signing up, you agree to the{' '}
+            <Text style={styles.link} onPress={openLink}>
+              User Notice
+            </Text>{' '}
+            and{' '}
+            <Text style={styles.link} onPress={openLink}>
+              Privacy Policy
+            </Text>
+            .
           </Text>
-          .
-        </Text>
 
-        <Text style={styles.link} onPress={openActionSheet}>
-          Can't log in our sign up?
-        </Text>
+          <Text style={styles.link} onPress={openActionSheet}>
+            Can't log in our sign up?
+          </Text>
+        </View>
       </View>
-    </View>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        handleComponent={null}
+        backdropComponent={renderBackdrop}
+        enableOverDrag={false}
+        enablePanDownToClose>
+        <BottomSheetView style={[styles.modalContainer]}>
+          <TouchableOpacity style={styles.modalBtn}>
+            <Ionicons name="mail-outline" size={20} />
+            <Text style={styles.btnText}>
+              {authType === ModalType.Login ? 'Log in' : 'Sign up'} with Email
+            </Text>
+          </TouchableOpacity>
+          {LOGIN_OPTIONS.map((option, index) => (
+            <TouchableOpacity key={index} style={styles.modalBtn}>
+              <Image source={option.icon} style={styles.btnIcon} />
+              <Text style={styles.btnText}>{option.text}</Text>
+            </TouchableOpacity>
+          ))}
+        </BottomSheetView>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
@@ -94,7 +173,7 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   btn: {
-    padding: 12,
+    padding: 10,
     borderRadius: 8,
     alignItems: 'center',
     borderColor: '#fff',
@@ -114,6 +193,23 @@ const styles = StyleSheet.create({
     fontSize: 12,
     textAlign: 'center',
     textDecorationLine: 'underline',
-    // textDecorationStyle: 'solid',
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
+    padding: 20,
+    gap: 20,
+  },
+  modalBtn: {
+    flexDirection: 'row',
+    gap: 14,
+    alignItems: 'center',
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+  btnIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
   },
 });
